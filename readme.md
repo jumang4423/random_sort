7ddf79e34bc32a43f6609ed43056fad629545949
jumang4423 <catontheskate4423@gmail.com>
2021-07-15 15:03:43 +0900

    finished

---
+++ readme.md
@@ -0,0 +1,15 @@
+# random sort
+
+randomly shuffle the array then find the sorted array.
+
+O(infinite?) which is sometimes really fast, everytime pretty slow.
+
+# usage
+
+put ```input_data``` then put random numbers like this.
+
+```
+1 2 3 47 7 7 4 5
+
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use rand::seq::SliceRandom;
+use std::fs;
+use std::str::FromStr;
+use std::vec::*;
 fn main() {
-    println!("Hello, world!");
+    println!("loading ./input_data...");
+
+    let buf = fs::read_to_string("input_data").unwrap();
+    let mv = buf.split_whitespace().map(|n| i32::from_str(n).unwrap());
+    let mut vec: Vec<i32> = Vec::new();
+
+    for d in mv {
+        vec.push(d);
+    }
+
+    println!("file loaded. calculating...");
+
+    loop {
+        let new_vec: Vec<i32> = vec
+            .choose_multiple(&mut rand::thread_rng(), vec.len())
+            .cloned()
+            .collect();
+
+        if check_sorted(&new_vec) {
+            println!("result is: {:?}", new_vec);
+            break;
+        }
+    }
+    println!("finished");
+}
+
+pub fn check_sorted(vec: &Vec<i32>) -> bool {
+    let mut min = i32::MIN;
+
+    for d in vec {
+        if min <= *d {
+            min = d.clone();
+        } else {
+            return false;
+        }
+    }
+
+    return true;
 }
